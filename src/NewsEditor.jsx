d34857230068571db63cdf982367ef53f4d055cb
fable0831 <fable0831@gmail.com>
2023-05-26 10:28:12 +0900

    createdAt 정렬

--- src/NewsEditor.jsx
+++ src/NewsEditor.jsx
@@ -1,5 +1,18 @@
-import { useState, useEffect } from 'react';
-import { Box, Heading, Button, Input, Textarea, FormControl, FormLabel, Select, Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react';
+import { useState, useEffect, useRef } from 'react';
+import {
+  Box,
+  Heading,
+  Button,
+  Input,
+  Textarea,
+  FormControl,
+  FormLabel,
+  Select,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
 import { v4 as uuidv4 } from 'uuid';
 import { firestore, storage } from './firebase';
 import { format } from 'date-fns';
@@ -8,59 +21,89 @@ function NewsEditor() {
   const [news, setNews] = useState([]);
   const [title, setTitle] = useState('');
   const [mainImage, setMainImage] = useState(null);
-  const [category, setCategory] = useState('all');
+  const [category, setCategory] = useState('');
   const [writer, setWriter] = useState('');
-  const [date, setDate] = useState('');
+  // const [date, setDate] = useState('');
   const [contents, setContents] = useState('');
   const [error, setError] = useState('');
+  const [selectedId, setSelectedId] = useState('');
+
+  // 파일 입력 참조를 위해 useRef를 사용
+  const fileInput = useRef();
+
+  const updateForm = (news) => {
+    setSelectedId(news.id);
+    setTitle(news.title);
+    setMainImage(null);
+    setCategory(news.category);
+    setWriter(news.writer);
+    // setDate(news.date);
+    setContents(news.contents);
+  };
 
   const addNews = async () => {
     try {
-      const id = uuidv4(); // unique한 id 생성
-      const imageName = mainImage.name;
+      if (selectedId) {
+        updateNews(selectedId, {
+          id: selectedId,
+          title,
+          mainImage,
+          category: category === '' ? 'all' : category,
+          writer,
+          // date,
+          contents,
+          createdAt: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+        });
+        setSelectedId('');
+      } else {
+        const id = uuidv4(); // unique한 id 생성
+        const imageName = mainImage.name;
 
-      // 이미지 크기 체크
-      if(mainImage.size > 10000000) {
-        setError('이미지 용량이 너무 큽니다. 10MB 이하의 이미지를 업로드해주세요.');
-        return;
+        // 이미지 크기 체크
+        if (mainImage.size > 10000000) {
+          setError('이미지 용량이 너무 큽니다. 10MB 이하의 이미지를 업로드해주세요.');
+          return;
+        }
+
+        // storage에 파일 업로드하기 - gs://dusmedia-e4bb8.appspot.com
+        const metadata = { customMetadata: { id } };
+        const imageRef = storage.ref(`images/${id}/${imageName}`);
+        await imageRef.put(mainImage, metadata);
+
+        // storage의 저장된 이미지 URL 추출하기
+        const imageURL = await imageRef.getDownloadURL();
+
+        // createdAt을 추가해 데이터 정렬
+        const createdAt = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+
+        // firestore에 뉴스 데이터 작성하기
+        const newNews = {
+          id,
+          title,
+          mainImage: imageURL,
+          category: category === '' ? 'all' : category,
+          writer,
+          // date,
+          contents,
+          createdAt,
+        };
+        await firestore.collection('news').doc(id).set(newNews);
+
+        // 뉴스 데이터 업데이트하고 폼 초기화하기
+        setNews([...news, newNews]);
+        setTitle('');
+        setMainImage(null);
+        setCategory('');
+        setWriter('');
+        // setDate('');
+        setContents('');
+        setError('');
+
+        fileInput.current.value = ''; // 파일 입력 초기화
       }
-
-      // storage에 파일 업로드하기 - gs://dusmedia-e4bb8.appspot.com
-      const metadata = { customMetadata: { id } };
-      const imageRef = storage.ref(`images/${id}/${imageName}`);
-      await imageRef.put(mainImage, metadata);
-
-      // storage의 저장된 이미지 URL 추출하기
-      const imageURL = await imageRef.getDownloadURL();
-
-      // createdAt을 추가해 데이터 정렬
-      const createdAt = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
-
-      // firestore에 뉴스 데이터 작성하기
-      const newNews = {
-        id,
-        title,
-        mainImage: imageURL,
-        category: category === "" ? 'all' : category,
-        writer,
-        date,
-        contents,
-        createdAt,
-      };
-      await firestore.collection('news').doc(id).set(newNews);
-
-      // 뉴스 데이터 업데이트하고 폼 초기화하기
-      setNews([...news, newNews]);
-      setTitle('');
-      setMainImage(null);
-      setCategory('');
-      setWriter('');
-      setDate('');
-      setContents('');
-      setError('');
     } catch (error) {
       console.error(error);
-      setError('뉴스 기사를 추가하는 도중 에러가 발생했습니다. 다시 시도해주세요.');
+      setError('뉴스 기사를 추가하는 도중 에러가 발생했습니다. 빈 항목을 작성해 주세요');
     }
   };
 
@@ -73,9 +116,11 @@ function NewsEditor() {
       setMainImage(null);
       setCategory('');
       setWriter('');
-      setDate('');
+      // setDate('');
       setContents('');
       setError('');
+
+      fileInput.current.value = ''; // 파일 입력 초기화
     } catch (error) {
       console.error(error);
       setError('뉴스 기사를 수정하는 도중 에러가 발생했습니다. 다시 시도해주세요.');
@@ -106,6 +151,8 @@ function NewsEditor() {
       snapshot.forEach((doc) => {
         data.push(doc.data());
       });
+      // createdAt 기준 내림차순 정렬
+      data.sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1));
       setNews(data);
     });
     return unsubscribe;
@@ -120,12 +167,12 @@ function NewsEditor() {
       </FormControl>
       <FormControl my="4">
         <FormLabel>메인 이미지</FormLabel>
-        <Input type="file" onChange={handleMainImageChange} accept="image/*" />
+        <Input type="file" ref={fileInput} onChange={handleMainImageChange} accept="image/*" />
       </FormControl>
       <FormControl isRequired my="4">
         <FormLabel>카테고리</FormLabel>
         <Select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="all" >전체</option>
+          <option value="all">전체</option>
           <option value="coin">암호화폐</option>
           <option value="stock">주식</option>
           <option value="overseas">해외증시</option>
@@ -135,11 +182,11 @@ function NewsEditor() {
       <FormControl isRequired my="4">
         <FormLabel>작성자</FormLabel>
         <Input type="text" value={writer} onChange={(e) => setWriter(e.target.value)} />
-      </FormControl>
-      <FormControl isRequired my="4">
+      </ FormControl>
+      {/* <FormControl isRequired my="4">
         <FormLabel>작성일</FormLabel>
         <Input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-      </FormControl>
+      </FormControl> */}
       <FormControl isRequired my="4">
         <FormLabel>뉴스 내용</FormLabel>
         <Textarea value={contents} height="600px" onChange={(e) => setContents(e.target.value)} />
@@ -157,7 +204,7 @@ function NewsEditor() {
       <Box textAlign="center" fontSize="18px" fontWeight="bold">
         {news.map((news) => (
           <Box key={news.id} borderBottom="1px solid #ccc" py="4">
-            <Box fontWeight="normal">{news.date}</Box>
+            <Box fontWeight="normal">{news.createdAt}</Box>
             <Box as="h2" fontSize="20px" mt="2">
               {news.title}
             </Box>
